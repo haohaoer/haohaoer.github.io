0460fa2a3626a07fa3e45b1c6bb14d762b8bb5e6
haohaoer <dinghao19990606@hotmail.com>
2024-02-24 02:49:06 +0900

    update sample.html

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
-   return [
-       {
-           name: 'Magnemite',
-           location: {
-               lat: 35.7094195,
-               lng: 139.7203878,
-           }
-       },
-   ];
+    return [{
+        name: 'chigusadai', location: {
+            lat: 35.5374048, lng: 139.5355570,
+        },
+    }];
 }
 
 function renderPlaces(places) {
@@ -22,17 +18,31 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.1 0.1 0.1');
-
-        model.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
-        });
-
-        scene.appendChild(model);
+        scene.appendChild(contributeModelTitle(latitude, longitude));
+        scene.appendChild(contributeModelInfo(latitude, longitude));
     });
+}
+
+const contributeModelTitle = (latitude, longitude) => {
+    let model1 = document.createElement('a-entity');
+    model1.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    model1.setAttribute('gltf-model', './chigusadai31.glb');
+    model1.setAttribute('position', '0 10 0');
+    model1.setAttribute('scale', '30 30 30');
+
+    model1.addEventListener('loaded', () => {
+        window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
+    });
+
+    return model1
+}
+
+const contributeModelInfo = (latitude, longitude) => {
+    let model2 = document.createElement('a-entity');
+    model2.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    model2.setAttribute('gltf-model', './chigusadai32.glb');
+    model2.setAttribute('position', '0 -20 0');
+    model2.setAttribute('scale', '30 30 30');
+
+    return model2
 }
