a6524ce0c19467016798c58e3d340dbc627987bd
haohaoer <dinghao19990606@hotmail.com>
2024-03-19 12:01:04 +0900

    add click

--- B.js
+++ B.js
@@ -4,17 +4,11 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
-    return [
-        {
-            name: 'chigusadai',
-            location: {
-                lat: 35.5374048,
-                lng: 139.5355570,
-//                lat: 35.7011277,
-//                lng: 139.6993951,
-            }
+    return [{
+        name: 'sightB', location: {
+            lat: 35.5374048, lng: 139.5355570,
         },
-    ];
+    }];
 }
 
 function renderPlaces(places) {
@@ -24,24 +18,36 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
-        let model1 = document.createElement('a-entity');
-        model1.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model1.setAttribute('gltf-model', './B1.glb');
-        model1.setAttribute('position', '0 10 0');
-        model1.setAttribute('scale', '20 20 20');
-
-        model1.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
+        scene.appendChild(contributeModelTitle(latitude, longitude));
+        scene.addEventListener('click', ()=>{
+            scene.appendChild(contributeModelInfo(latitude, longitude))
         });
-
-        let model2 = document.createElement('a-entity');
-        model2.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model2.setAttribute('gltf-model', './B2.glb');
-        model2.setAttribute('position', '0 -10 0');
-        model2.setAttribute('scale', '10 10 10');
-
-        scene.appendChild(model1);
-        scene.appendChild(model2);
-    });
+    })
 }
 
+const contributeModelTitle = (latitude, longitude) => {
+    let modelTitle = document.createElement('a-entity');
+    modelTitle.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    modelTitle.setAttribute('gltf-model', './B1.glb');
+    modelTitle.setAttribute('position', '0 3 0');
+    modelTitle.setAttribute('scale', '8 8 8');
+    modelTitle.setAttribute('rotation', '0 0 0')
+
+    modelTitle.addEventListener('loaded', () => {
+        window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
+    });
+
+
+    return modelTitle
+}
+
+const contributeModelInfo = (latitude, longitude) => {
+    let modelInfo = document.createElement('a-entity');
+    modelInfo.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    modelInfo.setAttribute('gltf-model', './B2.glb');
+    modelInfo.setAttribute('position', '0 -3 0');
+    modelInfo.setAttribute('scale', '3 3 3');
+    modelInfo.setAttribute('rotation', '0 0 0')
+
+    return modelInfo
+}
