9452a8601ea243f70cdc4dc42462544773aa1e3f
haohaoer <dinghao19990606@hotmail.com>
2024-03-19 15:47:11 +0900

    scale

--- B.js
+++ B.js
@@ -30,8 +30,8 @@ const contributeModelTitle = (latitude, longitude) => {
     let modelTitle = document.createElement('a-entity');
     modelTitle.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
     modelTitle.setAttribute('gltf-model', './B1.glb');
-    modelTitle.setAttribute('position', '0 4 0');
-    modelTitle.setAttribute('scale', '8 8 8');
+    modelTitle.setAttribute('position', '0 3 0');
+    modelTitle.setAttribute('scale', '5 5 5');
     modelTitle.setAttribute('rotation', '0 0 0')
 
     modelTitle.addEventListener('loaded', () => {
@@ -46,8 +46,8 @@ const contributeModelInfo = (latitude, longitude) => {
     let modelInfo = document.createElement('a-entity');
     modelInfo.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
     modelInfo.setAttribute('gltf-model', './B2.glb');
-    modelInfo.setAttribute('position', '0 -4 0');
-    modelInfo.setAttribute('scale', '3 3 3');
+    modelInfo.setAttribute('position', '0 -3 0');
+    modelInfo.setAttribute('scale', '1.5 1.5 1.5');
     modelInfo.setAttribute('rotation', '0 0 0')
 
     return modelInfo
