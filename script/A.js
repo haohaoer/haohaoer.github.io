b783ccd8016bab638e1e1908bb3f983a81d270d0
haohaoer <dinghao19990606@hotmail.com>
2024-03-21 03:50:44 +0900

    fix

--- script/A.js
+++ script/A.js
@@ -1,9 +1,7 @@
-import { contributeModel } from "./ContributeModel"
-
 const sightALocation = { lat: 35.7014561, lng: 139.7003657 }
-const scene = document.querySelector('a-scene')
 
 window.onload = () => {
+    const scene = document.querySelector('a-scene')
     const sightA = contributeSightA(sightALocation.lat, sightALocation.lng)
     addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
@@ -11,11 +9,12 @@ window.onload = () => {
     scene.appendChild(sightA)
 }
 
-export const onClickIntroductionAR = () => {
+const onClickIntroductionAR = () => {
+    const scene = document.querySelector('a-scene')
     scene.appendChild(contributeSightAIntro(sightALocation.lat, sightALocation.lng))
 }
 
-export const onClickIntroductionWindow = () => {
+const onClickIntroductionWindow = () => {
     alert("モチーフの天使は町の象徴で、\n「少しでも明るい光を照らすことができれば」\nとの願いが込められています。")
 }
 
@@ -30,4 +29,22 @@ const contributeSightAIntro = (latitude, longitude) => {
     const path = '/model/A2.glb'
     const position = { x: 0, y: -3, z: 0 }
     return contributeModel({ path, latitude, longitude, position })
+}
+
+const contributeModel = ({
+                             path,
+                             latitude,
+                             longitude,
+                             position = { x: 0, y: 0, z: 0 },
+                             scale = { x: 1, y: 1, z: 1 },
+                             rotation = { x: 0, y: 180, z: 0 },
+                         }) => {
+    const model = document.createElement('a-entity')
+    model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`)
+    model.setAttribute('gltf-model', path)
+    model.setAttribute('position', `${position.x} ${position.y} ${position.z}`)
+    model.setAttribute('scale', `${scale.x} ${scale.y} ${scale.z}`)
+    model.setAttribute('rotation', `${rotation.x} ${rotation.y} ${rotation.z}`)
+
+    return model
 }
