5f12f7ca2c4798516a9dc6d601f4010896cd89f1
haohaoer <dinghao19990606@hotmail.com>
2024-03-21 03:44:46 +0900

    紹介文ボタン追加　&& リファクタリング

--- script/A.js
+++ script/A.js
@@ -1,54 +1,33 @@
+import { contributeModel } from "./ContributeModel"
+
+const sightALocation = { lat: 35.7014561, lng: 139.7003657 }
+const scene = document.querySelector('a-scene')
+
 window.onload = () => {
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
-
-function staticLoadPlaces() {
-    return [{
-        name: 'sightA', location: {
-            lat: 35.7014561,
-            lng: 139.7003657,
-        },
-    }];
-}
-
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-
-        scene.appendChild(contributeModelTitle(latitude, longitude));
-        scene.addEventListener('click', ()=>{
-            scene.appendChild(contributeModelInfo(latitude, longitude))
-        });
-    })
-}
-
-const contributeModelTitle = (latitude, longitude) => {
-    let modelTitle = document.createElement('a-entity');
-    modelTitle.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    modelTitle.setAttribute('gltf-model', '/model/A1.glb');
-    modelTitle.setAttribute('position', '0 3 0');
-    modelTitle.setAttribute('scale', '5 5 5');
-    modelTitle.setAttribute('rotation', '0 180 0')
-
-    modelTitle.addEventListener('loaded', () => {
+    const sightA = contributeSightA(sightALocation.lat, sightALocation.lng)
+    addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
-    });
-
-
-    return modelTitle
+    })
+    scene.appendChild(sightA)
 }
 
-const contributeModelInfo = (latitude, longitude) => {
-    let modelInfo = document.createElement('a-entity');
-    modelInfo.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    modelInfo.setAttribute('gltf-model', '/model/A2.glb');
-    modelInfo.setAttribute('position', '0 -3 0');
-    modelInfo.setAttribute('scale', '1.5 1.5 1.5');
-    modelInfo.setAttribute('rotation', '0 180 0')
+export const onClickIntroductionAR = () => {
+    scene.appendChild(contributeSightAIntro(sightALocation.lat, sightALocation.lng))
+}
 
-    return modelInfo
+export const onClickIntroductionWindow = () => {
+    alert("モチーフの天使は町の象徴で、\n「少しでも明るい光を照らすことができれば」\nとの願いが込められています。")
+}
+
+const contributeSightA = (latitude, longitude) => {
+    const path = '/model/A1.glb'
+    const position = { x: 0, y: 3, z: 0 }
+    const scale = { x: 5, y: 5, z: 5 }
+    return contributeModel({ path, latitude, longitude, position, scale })
+}
+
+const contributeSightAIntro = (latitude, longitude) => {
+    const path = '/model/A2.glb'
+    const position = { x: 0, y: -3, z: 0 }
+    return contributeModel({ path, latitude, longitude, position })
 }
