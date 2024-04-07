c40bdb32c7a72bb507c44e5ba8d8624b13ec5332
C-uo <github@c-uo.dev>
2024-04-07 23:54:39 +0900

    add id

--- script/A.js
+++ script/A.js
@@ -1,32 +1,34 @@
 window.onload = () => {
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
+    let places = staticLoadPlaces()
+    renderPlaces(places)
+}
 
 function staticLoadPlaces() {
     let url = new URL(window.location.href)
     let params = url.searchParams
-    return [{
-        name: 'sightA', location: {
-            lat: params.get('latitude') ? params.get('latitude') : 35.7014561,
-            lng: params.get('longitude') ? params.get('longitude') : 139.7003657,
+    return [
+        {
+            name: 'sightA', location: {
+                lat: params.get('latitude') ? params.get('latitude') : 35.7014561,
+                lng: params.get('longitude') ? params.get('longitude') : 139.7003657,
+            },
         },
-    }];
+    ]
 }
 
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('a-scene')
 
     places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+        let latitude = place.location.lat
+        let longitude = place.location.lng
 
-        scene.appendChild(contributeModelTitle(latitude, longitude));
+        scene.appendChild(contributeModelTitle(latitude, longitude))
     })
 }
 
 const onClickIntroductionAR = () => {
-    let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('a-scene')
     const location = staticLoadPlaces().find(place => place.name === 'sightA').location
     scene.appendChild(contributeModelInfo(location.lat, location.lng))
 }
@@ -36,26 +38,28 @@ const onClickIntroductionWindow = () => {
 }
 
 const contributeModelTitle = (latitude, longitude) => {
-    let modelTitle = document.createElement('a-entity');
-    modelTitle.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    modelTitle.setAttribute('gltf-model', '/model/A1.glb');
-    modelTitle.setAttribute('position', '0 3 0');
-    modelTitle.setAttribute('scale', '5 5 5');
+    let modelTitle = document.createElement('a-entity')
+    modelTitle.setAttribute('id', 'sightA')
+    modelTitle.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`)
+    modelTitle.setAttribute('gltf-model', '/model/A1.glb')
+    modelTitle.setAttribute('position', '0 3 0')
+    modelTitle.setAttribute('scale', '5 5 5')
     modelTitle.setAttribute('rotation', '0 180 0')
 
     modelTitle.addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
-    });
+    })
 
     return modelTitle
 }
 
 const contributeModelInfo = (latitude, longitude) => {
-    let modelInfo = document.createElement('a-entity');
-    modelInfo.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    modelInfo.setAttribute('gltf-model', '/model/A2.glb');
-    modelInfo.setAttribute('position', '0 -3 0');
-    modelInfo.setAttribute('scale', '1.5 1.5 1.5');
+    let modelInfo = document.createElement('a-entity')
+    modelInfo.setAttribute('id', 'sightA-introduction')
+    modelInfo.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`)
+    modelInfo.setAttribute('gltf-model', '/model/A2.glb')
+    modelInfo.setAttribute('position', '0 -3 0')
+    modelInfo.setAttribute('scale', '1.5 1.5 1.5')
     modelInfo.setAttribute('rotation', '0 180 0')
     modelInfo.addEventListener('loaded', () => {
         window.dispatchEvent(new CustomEvent('gps-new-entity-place-loaded'))
