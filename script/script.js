fae2c573d7d227d097879976cd1c043a2919b453
C-uo <github@c-uo.dev>
2024-04-07 23:08:01 +0900

    フォーカスされた時に紹介文に切り替え
    - fix event

--- script/script.js
+++ script/script.js
@@ -4,9 +4,9 @@ const loadEntities = position => [
         type: 'a-entity',
         name: 'マーカーA',
         url: '/model/A1.glb',
-        location: {lat: position.latitude + 0.05, lng: position.longitude + 0.05},
-        position: {x: 0, y: 1.5, z: 0},
-        scale: {x: 30, y: 30, z: 30},
+        location: {lat: position.latitude + 0.1, lng: position.longitude + 0.1},
+        position: {x: 0, y: 0, z: 0},
+        scale: {x: 3, y: 3, z: 30},
     },
 ]
 
@@ -30,19 +30,15 @@ const setModel = (model, entity) => {
     if (model.rotation) entity.setAttribute('rotation', `${model.rotation.x} ${model.rotation.y} ${model.rotation.z}`)
 }
 
-AFRAME.registerComponent('show-introduction-on-mouseenter', {
-    schema: {
-        gltfModel: '/model/A2.glb',
-    },
-
-    init() {
-        const data = this.data
-        const el = this.el
-        this.el.addEventListener('mouseenter', () => {
-            console.log('mouseenter')
-            el.setAttribute('gltf-model', data.gltfModel)
+AFRAME.registerComponent('show-introduction-on-click', {
+    init: function () {
+        let data = this.data
+        let el = this.el
+        this.el.addEventListener('click', function (evt) {
+            console.log(evt.detail.intersection.point);
+            el.setAttribute('gltf-model', '/model/A2.glb')
         })
-    },
+    }
 })
 
 window.onload = () => {
@@ -52,7 +48,7 @@ window.onload = () => {
         loadEntities(position.coords).forEach((entity) => {
             console.log(entity)
             let model = contributeModel(entity)
-            model.setAttribute('show-introduction-on-mouseenter', '')
+            model.setAttribute('show-introduction-on-click', '')
             scene.appendChild(model)
         })
     }, (err) => console.error('Error in retrieving position', err), {
